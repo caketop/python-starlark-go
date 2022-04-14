81f49f9144d2944614b345e696866df6f14a5547
Jordan Webb <jordan@caketop.app>
2022-04-14 14:42:46 +0000

    Strict mode setup scripts

--- scripts/setup-linux.sh
+++ scripts/setup-linux.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
 if [ ! -f "/usr/bin/go" ]; then
-    curl -vvv -L -O https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
-    tar -xf go1.15.5.linux-amd64.tar.gz
+    curl -vvv -L -O https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+    tar -xf go1.18.1.linux-amd64.tar.gz
     mv go /usr/local
     ln -s /usr/local/go/bin/go /usr/bin/go
 fi
--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -xeuo pipefail
+
 if [ ! -f "/usr/local/bin/go" ]; then
-    curl -vvv -L -O https://golang.org/dl/go1.15.5.darwin-amd64.tar.gz
-    tar -xf go1.15.5.darwin-amd64.tar.gz
+    curl -vvv -L -O https://go.dev/dl/go1.18.1.darwin-amd64.tar.gz
+    tar -xf go1.18.1.darwin-amd64.tar.gz
     mv go /tmp
     ln -s /tmp/go/bin/go /usr/local/bin/go
 fi
