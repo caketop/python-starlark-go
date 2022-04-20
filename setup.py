01b063daaaa0f5dc20a2eab3a09d7409613d947f
Jordan Webb <jordan@caketop.app>
2022-04-20 09:48:13 -0500

    Move most things from setup.py to setup.cfg (#32)

--- setup.py
+++ setup.py
@@ -1,33 +1,8 @@
-import re
+from setuptools import Extension, setup
 
-import setuptools
+# I would only use setup.cfg but it can't compile extensions, so here we are.
 
-with open("src/pystarlark/__init__.py", "r", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pystarlark",
-    version=version,
-    author="Kevin Chung",
-    author_email="kchung@nyu.edu",
-    description="Python bindings for Starlark in Go",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ColdHeat/pystarlark",
-    package_dir={"": "src"},
-    packages=setuptools.find_packages("src"),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
-    # I'm not sure what this value is supposed to be
+setup(
     build_golang={"root": "github.com/caketop/pystarlark"},
-    package_data={"pystarlark": ["*.pyi", "py.typed"]},
-    ext_modules=[setuptools.Extension("pystarlark/starlark_go", ["starlark.go"])],
-    setup_requires=["setuptools-golang==2.7.0"],
+    ext_modules=[Extension("pystarlark/starlark_go", ["starlark.go"])],
 )
