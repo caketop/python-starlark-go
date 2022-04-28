859a5fabe757e1bb0e82a1575d2da6ec47a9901d
Jordan Webb <jordan@caketop.app>
2022-04-27 21:52:22 -0500

    no strip

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import Extension, setup
 # I would only use setup.cfg but it can't compile extensions, so here we are.
 
 setup(
-    build_golang={"root": "github.com/caketop/python-starlark-go"},
+    build_golang={"root": "github.com/caketop/python-starlark-go", "strip": False},
     ext_modules=[Extension("starlark_go/starlark_go", ["python_object.go"])],
 )
