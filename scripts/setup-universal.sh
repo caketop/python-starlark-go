ea6eca6a1bccd179a21be806b5e2d8aaeb445e2f
Jordan Webb <jordan@caketop.app>
2022-04-27 23:35:19 -0500

    try go from alpine and this is my last fucking offer to try and get this shit on musl

--- scripts/setup-universal.sh
+++ scripts/setup-universal.sh
@@ -1,37 +1,39 @@
 #!/bin/sh
 
-set -e
-set -x
+set -ex
 
 GO_VERSION=1.18.1
 
-if [ -e /etc/alpine-release ]; then
-  apk add bash curl git libc6-compat
-fi
-
-if [ -e /etc/debian_version ]; then
-  apt-get update
-  DEBIAN_FRONTEND=noninteractive apt-get install -y curl git
+if [ -e /etc/alpine-release ] && [ -z "$BASH_VERSION" ]; then
+  apk add bash curl git go libc6-compat
+  exit 0
 fi
 
 if [ -z "$BASH_VERSION" ]; then
   exec bash "$0"
 fi
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
+set -eou pipefail
 
-OLD_PATH=$PATH
+if [ -e /etc/debian_version ]; then
+  apt-get update
+  DEBIAN_FRONTEND=noninteractive apt-get install -y curl git
+fi
 
-# shellcheck disable=SC1090
-. ~/.asdf/asdf.sh
+install_go() {
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 
-asdf plugin add golang
-asdf install golang "$GO_VERSION"
+  # shellcheck disable=SC1090
+  . ~/.asdf/asdf.sh
 
-ln -s ~/.asdf/installs/golang/${GO_VERSION}/go/bin/go /usr/local/bin/go
-ln -s ~/.asdf/installs/golang/${GO_VERSION}/go/bin/gofmt /usr/local/bin/gofmt
+  asdf plugin add golang
+  asdf install golang "$GO_VERSION"
 
-PATH=$OLD_PATH
+  ln -s ~/.asdf/installs/golang/${GO_VERSION}/go/bin/go /usr/local/bin/go
+  ln -s ~/.asdf/installs/golang/${GO_VERSION}/go/bin/gofmt /usr/local/bin/gofmt
+}
+
+(install_go)
 
 go version
 
